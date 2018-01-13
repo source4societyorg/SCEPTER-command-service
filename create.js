34af627519996b024de374c38483bd04ba37be9f
Daniel Fredriksen <dfredriksen@cyint.technology>
2018-01-12 22:08:28 -0800

    Adding the service template into the create command

--- create.js
+++ create.js
@@ -1,58 +1,54 @@
 'use strict'
-
 const createServiceCommand = {
   command: 'service:create',
-  usage: 'service:create <service name> <target-repository> <[template]>',
-  description: 'Creates a new service in the service directory from an existing repository -- optionally forks it from another repository',
-  callback: callbackFunction,
-  serverlessCommand: serverlessFunction,
-  gitCommand: gitCommandFunction
+  usage: 'service:create <service-name> <git-repository>',
+  description: 'Use this to create a new service for your project as a submodule form the base SCEPTER template',
+  callback: function (args, credentials, command) {
+    const serviceName = args[3]
+    const gitRepository = args[4]
+    const forkRepository = "git@github.com:source4societyorg/SCEPTER-service-template.git"
+
+    this.serviceName = serviceName
+    this.gitRepository = gitRepository
+    this.forkRepository = forkRepository
+
+    if (typeof serviceName === 'undefined' || typeof gitRepository === 'undefined') {
+      command.printMessage('Usage: node bin/scepter ' + createServiceCommand.usage)
+      return
+    }
+
+    createServiceCommand.forkCommand(command)
+  },
+  forkCommand: forkFunction,
+  initializeService: initializeServiceFunction
 }
 
-function callbackFunction (args, credentials, command) {
-  const serviceName = args[3]
-  const targetRepository = args[4]
-  const template = args[5] || 'aws-nodejs'
-
-  this.serviceName = serviceName
-  this.targetRepository = targetRepository
-  this.template = template
-
-  if (typeof serviceName === 'undefined' || typeof targetRepository === 'undefined') {
-    command.printMessage('Usage: node bin/scepter ' + createServiceCommand.usage)
-    return
-  }
-
-  createServiceCommand.serverlessCommand(command)
-}
-
-function serverlessFunction (command) {
-  let execCommand = 'cd services; yarn sls create'
-  execCommand += ' --template ' + createServiceCommand.template
-  execCommand += ' --path services/' + createServiceCommand.serviceName
-
-  command.executeCommand(
-    execCommand,
-    'Successfully generated service configuration',
-    'Failed to generate service configuration',
-    createServiceCommand.gitCommand
-  )
-}
-
-function gitCommandFunction (command) {
-  let powershellModifier = ''
-  let simlinkCommand = 'ln -s ../../config/credentials.json ./credentials.json; ln -s ../../config/parameters.json ./parameters.json; ln -s ../../config/services.json ./services.json'
+function forkFunction (command) {
+  let commandString = ''
   let shell = typeof command.parameters !== 'undefined' ? command.parameters.shell : ''
   switch (shell) {
     case 'powershell':
-      powershellModifier = 'o'
-      simlinkCommand = 'cmd /c mklink credentials.json ..\\..\\config\\credentials.json; cmd /c mklink parameters.json ..\\..\\config\\services.json; cmd /c mklink parameters.json ..\\..\\config\\services.json'
+      commandString = 'o'
   }
-  const execCommand = 'cd services/' + createServiceCommand.serviceName + '; git init; git add .; git commit -m \'Initial commit\'; git remote add origin ' + createServiceCommand.targetRepository + '; git push -f origin master; ' + simlinkCommand + '; cd ../; rm -r -f' + powershellModifier + ' ' + createServiceCommand.serviceName + '; git submodule add --force ' + createServiceCommand.targetRepository + ' ' + createServiceCommand.serviceName
   command.executeCommand(
-    execCommand,
-    'Created new service',
-    'Failed to create new service'
+    'cd services; git clone ' + createServiceCommand.forkRepository + ' ' + createServiceCommand.serviceName + '; cd ' + createServiceCommand.serviceName + '; git remote rm origin; git remote add origin ' + createServiceCommand.gitRepository + '; git push origin master; cd ../; rm -r -f' + commandString + ' ' + createServiceCommand.serviceName + '; git submodule add ' + createServiceCommand.gitRepository + ' ' + createServiceCommand.serviceName,
+    'Successfully forked service template and added it to project',
+    'Failed to fork service template repository',
+    createServiceCommand.initializeService
+  )
+}
+
+function initializeServiceFunction (command) {
+  let commandString = ''
+  let shell = typeof command.parameters !== 'undefined' ? command.parameters.shell : ''
+  switch (shell) {
+    case 'powershell':
+      commandString = 'o'
+  }
+  command.executeCommand(
+    'cd services/' + createServiceCommand.serviceName + '; node config/initialize.js',
+    'Successfully created service',
+    'Failed to execute initialization script'
   )
 }
 
